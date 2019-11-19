31e32832d8e2f3c6436ac12b816a9f1470bee665
alexliu2360 <alexliu2360@126.com>
2019-11-19 15:01:39 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -15,46 +15,89 @@ import Contact from '@/views/Contact'
 Vue.use(Router)
 
 const routesMaps = [
-  // {
-  //   path:'/',
-  //   redirect:'/index'
-  //   // component: Main
-  // },
   {
     path:'/',
     redirect:'/home',
-    component:{
-      render (c) { return c('router-view') }
-    },
+    component: Main,
     children:[
       {
-        path:'home',
-        redirect:'home/index',
-        component:Main,
-        children: [
-          {
-            path: 'index',
-            name: 'index',
-            component: Home
-          },
-          {
-            path: 'about',
-            name: 'about',
-            component: About
-          },
-          {
-            path: 'products',
-            name: 'products',
-            component: Products
-          },
-          {
-            path: 'contact',
-            name: 'contact',
-            component: Contact
-          }
-        ]
+        path: 'home',
+        name: 'home',
+        component: Home
+      },
+      {
+        path: 'about',
+        name: 'about',
+        component: About
+      },
+      {
+        path: 'products',
+        name: 'products',
+        component: Products
+      },
+      {
+        path: 'contact',
+        name: 'contact',
+        component: Contact
       }
-    ]
+      ]
+  // },
+  // {
+  //   path:'/index',
+  //   redirect:'/index/home',
+  //   // component:{
+  //   //   render (c) { return c('router-view') }
+  //   // },
+  //   component:Main,
+  //   children:[
+  //     {
+  //       path: 'home',
+  //       name: 'home',
+  //       component: Home
+  //     },
+  //     {
+  //       path: 'about',
+  //       name: 'about',
+  //       component: About
+  //     },
+  //     {
+  //       path: 'products',
+  //       name: 'products',
+  //       component: Products
+  //     },
+  //     {
+  //       path: 'contact',
+  //       name: 'contact',
+  //       component: Contact
+  //     }
+  //     // {
+  //     //   path:'home',
+  //     //   redirect:'home/index',
+  //     //   component:Main,
+  //     //   children: [
+  //     //     {
+  //     //       path: 'index',
+  //     //       name: 'index',
+  //     //       component: Home
+  //     //     },
+  //     //     {
+  //     //       path: 'about',
+  //     //       name: 'about',
+  //     //       component: About
+  //     //     },
+  //     //     {
+  //     //       path: 'products',
+  //     //       name: 'products',
+  //     //       component: Products
+  //     //     },
+  //     //     {
+  //     //       path: 'contact',
+  //     //       name: 'contact',
+  //     //       component: Contact
+  //     //     }
+  //     //   ]
+  //     // }
+  //   ]
   }
 ]
 
